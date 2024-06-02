7935cb640d548ac0eb78b4ec5bbb72bba819e8ea
edmond-chow <45330596+edmond-chow@users.noreply.github.com>
2024-06-03 03:29:17 +0800

    Add files via upload

--- host/sedenion-analyser/Complex Testing Console/Base Extension.cpp
+++ host/sedenion-analyser/Complex Testing Console/Base Extension.cpp
@@ -43,13 +43,13 @@ namespace CmplxConExt
 			EM_JS(std::uint8_t, background, (), {
 				return iostream.getBackgroundColor().toConsoleColor();
 			});
-			EM_JS(wchar_t*, title, (), {
+			EM_JS(const wchar_t*, title, (), {
 				return getUTF32String(title, getTitle());
 			});
 			EM_JS(std::char_traits<wchar_t>::int_type, color_char_code, (std::uint8_t code), {
 				return Console.GetColorCharCode(code);
 			});
-			EM_ASYNC_JS(wchar_t*, read_line, (), {
+			EM_ASYNC_JS(const wchar_t*, read_line, (), {
 				let line = await iostream.readLine();
 				await iostream.writeLine(line, false);
 				return getUTF32String(__asyncjs__read_line, line);
@@ -90,7 +90,7 @@ namespace CmplxConExt
 				static std::wstring data = native::title();
 				return &data;
 			};
-			static wchar_t* read_line()
+			static const wchar_t* read_line()
 			{
 				return native::read_line();
 			};
@@ -113,87 +113,102 @@ namespace CmplxConExt
 			struct builder : public std::wstreambuf
 			{
 			public:
-				static constexpr const std::size_t out_sz = 1048576;
-				char_type out[out_sz];
+				static constexpr const int out_sz = 1048576;
+				static constexpr const int in_sz = 1024;
+				static constexpr const int back_sz = 32;
+				char_type out[out_sz + 1];
 				char_type* out_last;
+				char_type in[back_sz + in_sz];
+				const char_type* in_next;
 				bool end_slash;
-				bool can_sync;
-				explicit builder(std::size_t out_reserved) : out{}, out_last{ nullptr }, end_slash{ false }, can_sync{ false }
+				bool flush_now;
+				explicit builder() : out{}, out_last{ nullptr }, in{}, in_next{ nullptr }, end_slash{ false }, flush_now{ false }
 				{
-					if (out_reserved > out_sz) { out_reserved = out_sz; }
-					setp(out, out + out_sz - out_reserved);
+					this->setp(out, out + out_sz);
 				};
-				char_type* out_cur() const
-				{
-					return pptr();
-				};
-				void out_jmp(int offset)
-				{
-					pbump(offset);
-				};
+				friend class stream;
 			};
 			builder builder;
 		public:
-			explicit stream() : builder{ 1 } {};
+			explicit stream() : builder{}
+			{
+				this->setg(builder.in, builder.in + builder::back_sz, builder.in + builder::back_sz);
+			};
 		protected:
 			virtual int sync() override
 			{
-				char_type* out_cur = builder.out_cur();
-				if (builder.can_sync || out_cur == builder.out + builder::out_sz)
+				char_type* pptr = builder.pptr();
+				if (builder.flush_now || pptr == builder.out + builder::out_sz)
 				{
-					if (builder.out_last == nullptr) { builder.out_last = out_cur; }
+					if (builder.flush_now || builder.out_last == nullptr) { builder.out_last = pptr; }
 					else { ++builder.out_last; }
-					char_type out_last_poped = *builder.out_last;
+					char_type out_char = *builder.out_last;
 					*builder.out_last = L'\0';
 					console::write_code(builder.out);
-					*builder.out_last = out_last_poped;
-					std::copy(builder.out_last, out_cur, builder.out);
-					builder.out_jmp(builder.out - out_cur);
+					*builder.out_last = out_char;
+					std::copy(builder.out_last, pptr, builder.out);
+					builder.pbump(builder.out - builder.out_last);
 					builder.out_last = nullptr;
-					builder.end_slash = false;
-					builder.can_sync = false;
+					builder.flush_now = false;
 				}
 				return 0;
 			};
 		public:
 			int send()
 			{
-				builder.can_sync = true;
-				return sync();
+				builder.flush_now = true;
+				return this->sync();
 			};
 		protected:
 			virtual int_type overflow(int_type ch) override
 			{
-				sync();
+				this->sync();
 				if (ch != traits_type::eof())
 				{
-					char_type* out_cur = builder.out_cur();
-					*out_cur = traits_type::to_char_type(ch);
+					char_type* pptr = builder.pptr();
+					*pptr = traits_type::to_char_type(ch);
 					if (traits_type::to_char_type(ch) == L'\n')
 					{
-						builder.out_last = out_cur;
+						builder.out_last = pptr;
 						builder.end_slash = false;
 					}
 					else if (traits_type::to_char_type(ch) == L'\\')
 					{
-						if (builder.end_slash) { builder.out_last = out_cur; }
+						if (builder.end_slash) { builder.out_last = pptr; }
 						builder.end_slash = !builder.end_slash;
 					}
-					builder.out_jmp(1);
+					builder.pbump(1);
 				}
 				return ch;
 			};
 			virtual int_type underflow() override
 			{
-				send();
-				if (gptr() == egptr())
+				char_type* gptr = this->gptr();
+				if (gptr == this->egptr())
 				{
-					char_type* in = console::read_line();
-					std::size_t in_sz = wcslen(in);
-					setg(in, in, in + in_sz + 1);
-					in[in_sz] = L'\n';
+					char_type* nw_eback = builder.in;
+					std::copy(gptr - builder::back_sz, gptr, nw_eback);
+					if (builder.in_next == nullptr)
+					{
+						this->send();
+						builder.in_next = console::read_line();
+					}
+					const char_type* nw_next = builder.in_next;
+					while (nw_next < builder.in_next + builder::in_sz && *nw_next != L'\0') { ++nw_next; }
+					char_type* nw_gptr = nw_eback + builder::back_sz;
+					std::copy(builder.in_next, nw_next, nw_gptr);
+					int nw_offset = nw_next - builder.in_next;
+					if (*nw_next == L'\0')
+					{
+						nw_gptr[nw_offset++] = L'\n';
+						nw_next = nullptr;
+					}
+					char_type* nw_egptr = nw_gptr + nw_offset;
+					this->setg(nw_eback, nw_gptr, nw_egptr);
+					builder.in_next = nw_next;
+					gptr = nw_gptr;
 				}
-				return traits_type::to_int_type(*gptr());
+				return traits_type::to_int_type(*gptr);
 			};
 		};
 		static stream io{};
